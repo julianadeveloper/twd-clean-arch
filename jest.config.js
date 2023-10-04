1237b93958993ce1c8b763ff209364c68be88b6f
julianadeveloper <juhdeveloper@gmail.com>
2023-10-04 01:24:14 -0300

    chore: configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,11 @@
 module.exports = {
+  roots: [
+    '<rootDir>/src',
+  ],
   collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  moduleNameMapper: {
-    '@/teste/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
-
-  },
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
   transform: {
     '\\.ts$': 'ts-jest'
   }
