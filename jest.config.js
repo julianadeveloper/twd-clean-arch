49f8900637d74cfc8814d019d477ad64cb064676
julianadeveloper <juhdeveloper@gmail.com>
2023-10-04 01:16:09 -0300

    chore: add and configure jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/teste/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
