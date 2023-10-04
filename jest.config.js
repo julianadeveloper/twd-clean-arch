c7ae25e5a0e0a4049e87bc38fbd3c4008a1d15f5
julianadeveloper <juhdeveloper@gmail.com>
2023-10-04 01:49:29 -0300

    chore: configure jest to test new sources

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   roots: [
     '<rootDir>/src',
+    '<rootDir>/tests'
   ],
   collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/teste/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+
+  },
   transform: {
     '\\.ts$': 'ts-jest'
   }
